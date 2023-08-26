d7ef55c64d8998c67b41df7ed4bd4c50b84f05cf
linonetwo <linonetwo012@gmail.com>
2023-08-26 22:54:00 +0800

    feat: able to load empty wiki html

--- src/pages/WikiWebView/WikiViewer.tsx
+++ src/pages/WikiWebView/WikiViewer.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 import { styled } from 'styled-components/native';
+import { useTiddlyWiki } from './useTiddlyWiki';
 
 const WebViewContainer = styled.View`
   flex: 2;
+  height: 100%;
 `;
 
 export const WikiViewer = () => {
+  const wikiHTMLString = useTiddlyWiki();
   return (
     <WebViewContainer>
-      <WebView source={{ uri: 'https://reactnative.dev/' }} />
+      <WebView source={{ html: wikiHTMLString }} />
     </WebViewContainer>
   );
 };
