8b292fd6d907a87daa82e299eb762b41d0bbacbb
linonetwo <linonetwo012@gmail.com>
2023-08-30 21:55:34 +0800

    fix: onMessage can't be undefeind
    https://github.com/react-native-webview/react-native-webview/issues/1829#issuecomment-1699235643

--- src/pages/WikiWebView/WikiViewer.tsx
+++ src/pages/WikiWebView/WikiViewer.tsx
@@ -77,11 +77,13 @@ function WebViewWithPreload({ wikiHTMLString }: { wikiHTMLString: string }) {
     <WebView
       originWhitelist={['*']}
       source={{ html: wikiHTMLString }}
-      onMessage={onMessageReference.current}
+      onMessage={onMessageReference.current ?? ((message) => {
+        // this callback can't be undefined, see https://github.com/react-native-webview/react-native-webview/blob/dd31719f7b85e01e24ea2f9b2e7e9479fb51f26b/docs/Guide.md?plain=1#L424 and https://github.com/react-native-webview/react-native-webview/issues/1829#issuecomment-1699235643
+        console.log('WebView onMessage (before onMessageReference.current ready)', message);
+      })}
       ref={webViewReference}
       injectedJavaScriptBeforeContentLoaded={preloadScript}
-      // Open chrome://inspect/#devices to debug the WebView
-      webviewDebuggingEnabled
+      webviewDebuggingEnabled={true /* Open chrome://inspect/#devices to debug the WebView */}
     />
   );
 }
