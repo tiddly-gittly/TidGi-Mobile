4fddea76851e2bf3d31b3e18443b1b220566b442
linonetwo <linonetwo012@gmail.com>
2023-08-31 15:17:17 +0800

    feat: allow paste url to import

--- src/pages/Importer/Index.tsx
+++ src/pages/Importer/Index.tsx
@@ -3,14 +3,23 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { BarCodeScannedCallback, BarCodeScanner } from 'expo-barcode-scanner';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
+import { styled } from 'styled-components/native';
 import { RootStackParameterList } from '../../App';
 import { useImportHTML } from './useImportHTML';
+
+const Container = styled.View`
+  flex: 1;
+  padding: 20px;
+  height: 100%;
+  overflow-y: scroll;
+`;
 
 export const Importer: FC<StackScreenProps<RootStackParameterList, 'Importer'>> = ({ navigation }) => {
   const { t } = useTranslation();
   const [hasPermission, setHasPermission] = useState<undefined | boolean>();
-  const [scanned, setScanned] = useState(false);
+  const [qrScannerOpen, setQrScannerOpen] = useState(false);
+  const [scannedString, setScannedString] = useState('');
   const [wikiUrl, setWikiUrl] = useState<undefined | URL>();
 
   useEffect(() => {
@@ -26,14 +35,23 @@ export const Importer: FC<StackScreenProps<RootStackParameterList, 'Importer'>> 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (type === BarCodeScanner.Constants.BarCodeType.qr) {
       try {
-        const url = new URL(data);
-        setScanned(true);
-        setWikiUrl(url);
+        setQrScannerOpen(false);
+        setScannedString(data);
       } catch (error) {
         console.warn('Not a valid URL', error);
       }
     }
   }, []);
+  useEffect(() => {
+    if (scannedString !== '') {
+      try {
+        const url = new URL(scannedString);
+        setWikiUrl(url);
+      } catch (error) {
+        console.warn('Not a valid URL', error);
+      }
+    }
+  }, [scannedString]);
 
   const { error: importError, status: importStatus, storeHtml, importedWikiWorkspace } = useImportHTML();
 
@@ -45,67 +63,60 @@ export const Importer: FC<StackScreenProps<RootStackParameterList, 'Importer'>> 
   }
 
   return (
-    <View style={styles.container}>
-      {!scanned && (
+    <Container>
+      {qrScannerOpen && (
         <BarCodeScanner
           // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr as string]}
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
+          onBarCodeScanned={handleBarCodeScanned}
         />
       )}
-      {scanned && wikiUrl !== undefined && (
+      <Button
+        onPress={() => {
+          setQrScannerOpen(true);
+        }}
+      >
+        <Text>Open QRCode Scanner</Text>
+      </Button>
+      <TextInput
+        value={scannedString}
+        onChangeText={(newText: string) => {
+          setScannedString(newText);
+        }}
+      />
+      {!qrScannerOpen && wikiUrl !== undefined && (
         <>
           <Button
-            title={'Tap to Scan Again'}
-            onPress={() => {
-              setScanned(false);
-              setWikiUrl(undefined);
-            }}
-          />
-          <Button
-            title={t('Import.ImportWiki', { wikiUrl: `${wikiUrl.host}:${wikiUrl.port}` })}
             onPress={async () => {
               await storeHtml(wikiUrl.href, 'wiki');
               setWikiUrl(undefined);
             }}
-          />
+          >
+            <Text>{t('Import.ImportWiki', { wikiUrl: `${wikiUrl.host}:${wikiUrl.port}` })}</Text>
+          </Button>
           <Text>{importStatus}</Text>
         </>
       )}
-      {importStatus === 'error' && (
-        <>
-          <Text>{importError}</Text>
-          <Button
-            title={'Tap to Scan Again'}
-            onPress={() => {
-              setScanned(false);
-              setWikiUrl(undefined);
-            }}
-          />
-        </>
-      )}
-      {importStatus !== 'idle' && importStatus !== 'error' && (
-        <>
-          <Text>{importStatus}</Text>
-          {importedWikiWorkspace !== undefined && (
-            <Button
-              title={`${t('Open')} ${importedWikiWorkspace.name}`}
-              onPress={() => {
-                navigation.navigate('WikiWebView', { id: importedWikiWorkspace.id });
-              }}
-            />
-          )}
-        </>
-      )}
-    </View>
+      {importStatus === 'error'
+        ? (
+          <>
+            <Text>{importError}</Text>
+          </>
+        )
+        : (
+          <>
+            <Text>{importStatus}</Text>
+            {importedWikiWorkspace !== undefined && (
+              <Button
+                onPress={() => {
+                  navigation.navigate('WikiWebView', { id: importedWikiWorkspace.id });
+                }}
+              >
+                <Text>{`${t('Open')} ${importedWikiWorkspace.name}`}</Text>
+              </Button>
+            )}
+          </>
+        )}
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-});
