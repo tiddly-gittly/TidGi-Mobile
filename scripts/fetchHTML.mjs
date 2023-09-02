ba76ee3ee76ee89ddffd288837e8e182f689f101
linonetwo <linonetwo012@gmail.com>
2023-09-03 00:41:13 +0800

    feat: count bytes

--- scripts/fetchHTML.mjs
+++ scripts/fetchHTML.mjs
@@ -1,10 +1,10 @@
 // import {escape, unescape} from 'html-escaper';
 
-const host = '192.168.3.15:5212'
+const host = '192.168.3.15:5212';
 const html = await fetch(`http://${host}/tw-mobile-sync/get-skinny-html`).then(response => response.text());
 const jsonscript = await fetch(`http://${host}/tw-mobile-sync/get-skinny-tiddlywiki-tiddler-store-script`).then(response => response.text());
-console.log(html.length);
-console.log(jsonscript.length);
+console.log(Buffer.byteLength(html));
+console.log(Buffer.byteLength(jsonscript));
 fs.writeFileSync('skinny.html', html);
 fs.writeFileSync('jsonscript.html', jsonscript);
 
