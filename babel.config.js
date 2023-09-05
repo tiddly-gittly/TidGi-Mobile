a047f1026a87fb263223420f097789279e734add
linonetwo <linonetwo012@gmail.com>
2023-09-06 01:18:22 +0800

    fix: Cannot read property 'getItem' of undefined
    https://github.com/facebook/react-native/issues/36828#issuecomment-1706987942

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    plugins: ['@babel/plugin-transform-flow-strip-types', ['@babel/plugin-transform-private-methods', { loose: true }]],
   };
 };
