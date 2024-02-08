b7ae7192cea0d457ec81c592fa1bc85c86ca29b3
linonetwo <linonetwo012@gmail.com>
2024-02-08 18:48:02 +0800

    Update fit-f-droid.mjs

--- scripts/fit-f-droid.mjs
+++ scripts/fit-f-droid.mjs
@@ -6,5 +6,5 @@ const projectRoot = path.join(__dirname, '..');
 const expoLocationPath = path.join(projectRoot, 'node_modules', 'expo-location');
 const expoApplicationPath = path.join(projectRoot, 'node_modules', 'expo-application');
 
-await Promise.all([fs.unlink(expoLocationPath), fs.unlink(expoApplicationPath)]);
+await Promise.all([fs.remove(expoLocationPath), fs.remove(expoApplicationPath)]);
 await Promise.all([fs.copy(path.join(projectRoot, 'libs', 'expo-location'), expoLocationPath), fs.copy(path.join(projectRoot, 'libs', 'expo-application'), expoApplicationPath)]);
