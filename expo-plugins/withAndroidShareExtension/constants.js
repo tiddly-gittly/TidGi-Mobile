a172698338a93d1ca26c0fe1cf97d1effbb64c35
lin onetwo <linonetwo012@gmail.com>
2024-03-07 21:55:45 +0800

    refactor: remove old share intent lib

--- expo-plugins/withAndroidShareExtension/constants.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Plugin created for Expo Share Intent Demo (https://github.com/achorein/expo-share-intent-demo)
- * author: achorein (https://github.com/achorein)
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
