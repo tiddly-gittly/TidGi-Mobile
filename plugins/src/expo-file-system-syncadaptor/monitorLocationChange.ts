f17190c7c70ba62c679a60eac6376a867727c26c
linonetwo <linonetwo012@gmail.com>
2024-03-19 13:48:04 +0800

    fix: window is not defined in monitorLocationChange

--- plugins/src/expo-file-system-syncadaptor/monitorLocationChange.ts
+++ plugins/src/expo-file-system-syncadaptor/monitorLocationChange.ts
@@ -1,4 +1,11 @@
 const setup = () => {
+  if (typeof window === 'undefined') {
+    setTimeout(() => {
+      console.error('window is not defined in monitorLocationChange, retry in 1s.');
+      setup();
+    }, 1000);
+    return;
+  }
   window.addEventListener('hashchange', () => {
     const lastLocationHash = location.hash;
     window.service?.wikiHookService?.saveLocationInfo(lastLocationHash);
