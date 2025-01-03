ed188b9d5e1b819bfae807ae0c8546852faea992
lin onetwo <linonetwo012@gmail.com>
2025-01-04 00:54:16 +0800

    fix: allow sync deck tiddler and plugin tiddlers
    fixes #77

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,17 +1,15 @@
 /**
  * -[is[binary]]: binary tiddlers are loaded later as file. This filter is used to get text that will be saved into SQLite.
- * // TODO: remove until https://github.com/Jermolene/TiddlyWiki5/pull/7927 merged
- * -[type[application/msword]]: seems they are not recognized as binary tiddlers properly, not sure, remove if I'm wrong...
  */
-export const defaultNonBinaryFilter = ' -[is[binary]] -[type[application/msword]] -[type[application/excel]] -[type[application/mspowerpoint]] -[type[application/vnd.ms-excel]]';
+export const defaultNonBinaryFilter = ' -[is[binary]]';
 /**
  * Filter all user's binary files.
  * I tested, this won't include system tiddlers. (or maybe system tiddlers currently don't have binary file?)
  */
 export const defaultBinaryFilter = defaultNonBinaryFilter.replaceAll(' -', '');
 /**
- * [!is[system]]: user tiddlers
+ * [!prefix[$:/core]]: user tiddlers and updated plugins and configs for plugins, like TidMe's `$:/Deck/`
  * -[type[application/javascript]]: javascript tiddlers must be preload, so is already being synced by `/tw-mobile-sync/get-skinny-tiddlywiki-tiddler-store-script`
  */
-export const defaultNonPreloadedTiddlerFilter = '[!is[system]] -[type[application/javascript]]';
+export const defaultNonPreloadedTiddlerFilter = '[!prefix[$:/core]] -[type[application/javascript]]';
 export const defaultTextBasedTiddlerFilter = `${defaultNonPreloadedTiddlerFilter}${defaultNonBinaryFilter}`;
