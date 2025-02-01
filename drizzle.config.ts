59423e28a59ad553e6284e47d6b9291096dbaaec
linonetwo <linonetwo012@gmail.com>
2025-02-02 01:49:56 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,5 @@ export default {
   schema: 'src/services/SQLiteService/orm/index.ts',
   out: 'src/services/SQLiteService/orm/migrations',
   driver: 'expo',
+  dialect: 'sqlite',
 } satisfies Config;
