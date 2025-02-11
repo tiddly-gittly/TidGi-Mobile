6f64337c860c830e3e573c21ec627641cf2438e8
lin onetwo <linonetwo012@gmail.com>
2025-02-12 00:30:30 +0800

    fix: unused

--- scripts/fix-gradle.mjs
+++
@@ -1,5 +0,0 @@
-/**
- * Fix No signature of method: org.gradle.internal.extensibility.DefaultExtraPropertiesExtension.kotlinVersion() is applicable for argument types: () values: []
- * @url https://github.com/expo/expo/issues/24945#issuecomment-1914306405
- */
-await $`find ./node_modules -name "build.gradle" -exec sed -i 's/kotlinVersion()/kotlinVersion/g' {} +`;
